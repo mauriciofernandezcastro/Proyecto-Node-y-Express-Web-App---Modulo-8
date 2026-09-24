0e3f5a5de14e16618670f3cfb013d13462706950
Mauricio Fernandez <77501857+mauriciofernandezcastro@users.noreply.github.com>
2026-09-23 23:05:35 -0300

    Define Usuario model with Sequelize

--- usuario.js
+++ usuario.js
@@ -1,1 +1,11 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
+const Usuario = sequelize.define('Usuario', {
+  nombre: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  foto: { type: DataTypes.STRING }
+});
+
+module.exports = Usuario;
