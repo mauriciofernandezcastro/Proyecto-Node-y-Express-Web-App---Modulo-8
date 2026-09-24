4e50833d0598850a66671be5d7894a7c854c91c9
Mauricio Fernandez <77501857+mauriciofernandezcastro@users.noreply.github.com>
2026-09-23 23:12:08 -0300

    Add user routes with token verification

--- usuarios.routes.js
+++ usuarios.routes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/usuarios.controller');
+const verificarToken = require('../middlewares/verificarToken');
 
+router.get('/', verificarToken, ctrl.getAll);
+router.get('/perfil', verificarToken, ctrl.perfil);
+router.get('/:id', verificarToken, ctrl.getOne);
+router.put('/:id', verificarToken, ctrl.update);
+router.delete('/:id', verificarToken, ctrl.delete);
+
+module.exports = router;
