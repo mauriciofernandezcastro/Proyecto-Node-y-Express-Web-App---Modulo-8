a4684cf3ed9461f02c0b9f2907ed1038b0cc4ce7
Mauricio Fernandez <77501857+mauriciofernandezcastro@users.noreply.github.com>
2026-09-23 23:06:42 -0300

    Define Producto model with associations to Usuario

--- producto.js
+++ producto.js
@@ -1,1 +1,14 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Usuario = require('./usuario');
 
+const Producto = sequelize.define('Producto', {
+  nombre: { type: DataTypes.STRING, allowNull: false },
+  precio: { type: DataTypes.FLOAT, allowNull: false },
+  descripcion: { type: DataTypes.STRING }
+});
+
+Usuario.hasMany(Producto, { foreignKey: 'usuarioId' });
+Producto.belongsTo(Usuario, { foreignKey: 'usuarioId' });
+
+module.exports = Producto;
