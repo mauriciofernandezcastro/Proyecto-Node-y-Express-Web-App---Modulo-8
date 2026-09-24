a6b52061e5102c7356097b2e958892c6ff0bc28a
Mauricio Fernandez <77501857+mauriciofernandezcastro@users.noreply.github.com>
2026-09-23 23:07:32 -0300

    Add token verification middleware using JWT

--- verificarToken.js
+++ verificarToken.js
@@ -1,1 +1,20 @@
+const jwt = require('jsonwebtoken');
 
+function verificarToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ status: 'error', message: 'Token requerido' });
+  }
+
+  jwt.verify(token, process.env.SECRET_JWT, (err, user) => {
+    if (err) {
+      return res.status(403).json({ status: 'error', message: 'Token invalido o expirado' });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = verificarToken;
